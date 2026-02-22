229127072cf95658142ecb189f280be45de25dc1
Hadi Khalilpour <hadi.vbp@gmail.com>
2026-02-22 12:04:45 +0330

    add delete feature

--- my_diary/app.py
+++ my_diary/app.py
@@ -1,3 +1,4 @@
+import time
 from flask import Flask, render_template, request, redirect
 import json, os
 
@@ -24,17 +25,41 @@ notes=load_notes()
 
 @app.route('/')
 def home():
-    return render_template('index.html',notes=notes)
+    with open('notes.json', 'r') as f:
+        notes = json.load(f)
 
-@app.route('/add', methods=['GET', 'POST'])
+    return render_template('index.html', notes=notes)
+
+@app.route('/add', methods=['POST'])
 def add_note():
-    if request.method == 'POST':
-        text = request.form.get('note' ,'').strip()
-        if text:
-            notes.append(text)
-            save_notes()
-        return redirect('/')
-    return render_template('add.html')
+    note_text = request.form['note']
+
+    with open('notes.json', 'r') as f:
+        notes = json.load(f)
+
+    new_note = {
+        "id": int(time.time() * 1000),
+        "text": note_text
+    }
+
+    notes.append(new_note)
+
+    with open('notes.json', 'w') as f:
+        json.dump(notes, f)
+
+    return redirect('/')
+
+@app.route('/delete/<int:note_id>', methods=['POST'])
+def delete_note(note_id):
+    with open('notes.json', 'r') as f:
+        notes = json.load(f)
+
+        notes = [note for note in notes if note["id"] != note_id]
+
+    with open('notes.json', 'w') as f:
+        json.dump(notes, f)
+
+    return redirect('/')
 
 if __name__ == '__main__':
     from waitress import serve
